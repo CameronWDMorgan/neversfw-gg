cce5878ab076f5cfc19a8ea003046cb6a9a2cb7e
CameronWDMorgan <cameronwdmorgan@gmail.com>
2023-11-08 11:33:46 +0000

    Fix Fix

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const rateLimit = require('express-rate-limit')
 
 const limiter = rateLimit({
 	windowMs: (1 * 60 * 1000),
-	max: 5000,
+	max: 1000,
 	standardHeaders: true,
 	legacyHeaders: false,
 })
