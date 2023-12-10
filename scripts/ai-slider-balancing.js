27c41521c7b4a99e23df9fc5842c63c3cf343b3c
CameronWDMorgan <cameronwdmorgan@gmail.com>
2023-12-10 02:27:55 +0000

    /ai Update CSS and JavaScript files, maxCounts

--- scripts/ai-slider-balancing.js
+++ scripts/ai-slider-balancing.js
@@ -1,12 +1,16 @@
 function updateSliderDisplay() {
     // Update the displayed value for steps and quantity
-    document.getElementById('stepsValue').innerText = document.getElementById('steps').value;
-    document.getElementById('quantityValue').innerText = document.getElementById('quantity').value;
+    stepsValue = document.getElementById('steps').value
+    stepsMax = document.getElementById('steps').max
+    quantityValue = document.getElementById('quantity').value
+    quantityMax = document.getElementById('quantity').max
+    document.getElementById('stepsValue').innerText = `${stepsValue}/${stepsMax}`
+    document.getElementById('quantityValue').innerText = `${quantityValue}/${quantityMax}`
 }
 
 function adjustImageQuantity() {
     const steps = document.getElementById('steps').value;
-    const stepsProportion = (steps - 20) / (maxStepsCountWanted - 20);
+    const stepsProportion = (steps - 10) / (maxStepsCountWanted - 10);
     
     // Adjust max image quantity based on steps proportion
     const maxImageQuantity = Math.round(maxImageQuantityWanted - stepsProportion * (maxImageQuantityWanted - 1));
@@ -23,7 +27,7 @@ function adjustSteps() {
     const imageProportion = (imageQuantity - 1) / (maxImageQuantityWanted - 1);
     
     // Adjust max steps based on image quantity proportion
-    const maxSteps = Math.round(maxStepsCountWanted - imageProportion * (maxStepsCountWanted - 20));
+    const maxSteps = Math.round(maxStepsCountWanted - imageProportion * (maxStepsCountWanted - 10));
     const stepsInput = document.getElementById('steps');
     stepsInput.max = maxSteps;
     
